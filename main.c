ab141bfbfc6967fbf84efffc2565cfad21361289
kylelitter <89498126+kylelitter@users.noreply.github.com>
2021-08-25 11:15:25 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include "head.h"
 
 void main(void)
@@ -7,4 +8,7 @@ void main(void)
   int tp1 = add(it1, it2);
   int tp2 = sub(it2, it1);
   int tp3 = complex(it1, it2);
+  printf("The tp1 is %d \n", tp1);
+  printf("The tp2 is %d \n", tp2);
+  printf("The tp3 is %d \n", tp3);
 }
