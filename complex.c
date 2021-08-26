9ae0dc64ab19bc3782fb39207e7d2a2157b68659
Kyle <leiz.hust@aliyun.com>
2021-08-26 10:14:01 +0800

    [CHG-TEST01]Kyle

--- complex.c
+++ complex.c
@@ -4,5 +4,6 @@ int complex(int inter1, int inter2)
 {
   int tp1 = add(inter1, inter2);
   int tp2 = sub(inter2, inter2);
+  tp2 = tp1*tp2 - tp2%tp1; /*Added by Kyle for CHG-TEST01*/
   return (tp2 - tp1);
 }
