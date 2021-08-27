d6e3c9635d6833f4df37765ffc54253c5d6b34c7
LAPTOP-1S913LCN\kyle <leiz.hust@aliyun.com>
2021-08-27 19:56:29 +0800

    [CHG-TEST05]Kyle Commit - 3

--- complex.c
+++ complex.c
@@ -13,6 +13,8 @@ int complex(int inter1, int inter2)
   int tp3 = tp2*tp1 + tp1%tp2 - tp2%tp1;
   tp2 = (tp3 + tp2)%tp1;
   /*Begin added by Kyle for CHG-TEST05*/
+
+  tp1 = (tp2 - tp1)/tp3;  /*Added by Kyle for CHG-TEST04*/
   
   return (tp2 - tp1);
 }
