6499440b56b9e92219d100565d7e1464ce107cdd
LAPTOP-1S913LCN\kyle <leiz.hust@aliyun.com>
2021-08-27 20:08:12 +0800

    [CHG-TEST05]

--- complex.c
+++ complex.c
@@ -15,6 +15,7 @@ int complex(int inter1, int inter2)
   /*Begin added by Kyle for CHG-TEST05*/
 
   tp1 = (tp2 - tp1)/tp3;  /*Added by Kyle for CHG-TEST05*/
+  tp2 = tp2*tp1*(tp2 + tp1)%tp1; /*Added by Kyle for CHG-TEST05*/
   
   return (tp2 - tp1);
 }
